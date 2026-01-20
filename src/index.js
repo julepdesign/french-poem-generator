529083e842b91251f70fbc465732988c0697d01b
julepdesign <julepgraphicdesign@gmail.com>
2026-01-20 12:42:45 +0100

    added Ai API integration

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ textWrapper.innerHTML = textWrapper.textContent
     (word) =>
       `<span class="word">${word.replace(
         /\S/g,
-        "<span class='letter'>$&</span>"
-      )}</span>`
+        "<span class='letter'>$&</span>",
+      )}</span>`,
   )
   .join(" ");
 
@@ -31,9 +31,25 @@ anime
 
 function generatePoem(event) {
   event.preventDefault();
-  let poemElement = document.querySelector("#poem");
-  poemElement.innerHTML = "displaying poem...";
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "c5a77b10o404035t8533ef4bf02afcfc";
+  let prompt = `User instructions: generate a short poem in french language about ${instructionsInput.value}`;
+  let context =
+    "You are a romantic poem expert. You like to write short poem in french language. Your mission is to generate a maximum 4 lines poem with a br tag for a line break between each lines. Make sure to follow the user instructions. Sign the poem with <em>SheCodes AI<em> with two break lines at the end of the poem";
+
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
+function displayPoem(response) {
+  console.log("poem generated");
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
